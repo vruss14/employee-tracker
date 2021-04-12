145e091c285d92a448824e1af7baf698b0e67323
Valerie Russell <vruss14@gmail.com>
2021-04-12 19:40:59 -0400

    created basic functions and questions, and accounted for possible user errors

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const actionOptions = ["Add a department", "Add a role", "Add an employee", "View a department", "View a role", "View an employee", "Update an employee's role", "Quit"];
+// Answer lists are organized into arrays for easy access
+
+const actionOptions = ["Add a department", "Add a role", "Add an employee", "View a department", 
+"View a role", "View an employee", "Update an employee's role", "Quit"];
+
+const yesOrNo = ["Yes", "No"];
+
+// These arrays should be modified based on user responses and currently have placeholder data for testing purposes
+
+const departments = ["Engineering", "Sales", "Marketing"];
+const roles = ["Engineer", "Intern", "Manager"];
+const employees = ["John Doe", "Jane Doe"];
+
+
+// This is the main menu
 
 function askForAction () {
     inquirer.prompt([
@@ -36,33 +50,247 @@ function askForAction () {
     })
 }
 
+// This is the function that will be called when the application is initially run (i.e. the main menu)
 askForAction();
 
-function askAddDepartmentQuestions() {
-    console.log("You want to add a department.");
+// These questions and verifications are used if the user wants to add a department
 
+function askAddDepartmentQuestions() {
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'department',
+        },
+
+    ])
+
+    .then (function(response) {
+
+        if (response.department === "") {
+            console.log("Your response cannot be blank.");
+            askAddDepartmentQuestions();
+            return;
+        }
+
+        inquirer.prompt([
+
+            {
+                type: 'list',
+                message: `You want to create a department named ${response.department}. Is this correct?`,
+                name: 'departmentverify',
+                choices: yesOrNo
+            },
+        ])
+
+        .then (function(response) {
+            if(response.departmentverify === "Yes") {
+                console.log ("Great! We will continue.");
+            } else {
+                askAddDepartmentQuestions();
+            }
+        })
+    })
 }
+
+// These questions clarify the role the user wants to add, and ensures that no errors have been made
 
 function askAddRoleQuestions() {
-    console.log("You want to add a role.");
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'What is the title of the role?',
+            name: 'role',
+        },
+
+        {
+            type: 'input',
+            message: 'What is the salary of the role? Please enter the value as a number only, with no special characters.',
+            name: 'rolesalary',
+        },
+
+        {
+            type: 'input',
+            message: 'What department does this role belong to?',
+            name: 'roledept',
+        },
+    ])
+
+    .then (function(response) {
+
+        // Ensures that all three questions have been answered properly before proceeding.
+
+        if (response.role === "") {
+            console.log("The title of the role cannot be blank.");
+            askAddRoleQuestions();
+            return;
+        } else if (response.rolesalary === "") {
+            console.log("The salary cannot be blank.");
+            askAddRoleQuestions();
+            return;
+        } else if (isNaN(response.rolesalary)) {
+            console.log("Please enter a valid salary.");
+            askAddRoleQuestions();
+            return;
+        } else if (response.roledept === "") {
+            console.log("The department cannot be blank.");
+            askAddRoleQuestions();
+            return;
+        }
+        
+        inquirer.prompt([
+
+            {
+                type: 'list',
+                message: `You want to create a role named ${response.role}, which pays $${response.rolesalary} annually and belongs to the ${response.roledept} department. Is this correct?`,
+                name: 'roleverify',
+                choices: yesOrNo
+            },
+        ])
+
+        .then (function(response) {
+            if(response.roleverify === "Yes") {
+                console.log ("Great! We will continue.");
+            } else {
+                askAddRoleQuestions();
+            }
+        })
+    })
 }
 
+// This function asks for relevant information about the employee being added to the database, and accounts for error handling
+
 function askAddEmployeeQuestions() {
-    console.log("You want to add an employee.");
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: "What is the employee's first name?",
+            name: 'employeefirstname',
+        },
+
+        {
+            type: 'input',
+            message: "What is the employee's last name?",
+            name: 'employeelastname',
+        },
+
+        {
+            type: 'input',
+            message: "What is the employee's role?",
+            name: 'employeerole',
+        },
+
+        {
+            type: 'input',
+            message: "Who is the employee's manager? If the employee has no manager, leave this blank and hit enter.",
+            name: 'employeemanager',
+        },
+    ])
+
+    .then (function(response) {
+
+        if (response.employeefirstname === "") {
+            console.log("The employee's first name cannot be blank.");
+            askAddEmployeeQuestions();
+            return;
+        } else if (response.employeelastname === "") {
+            console.log("The employee's last name cannot be blank.");
+            askAddEmployeeQuestions();
+            return;
+        } else if (isNaN(response.employeerole === "")) {
+            console.log("The employee's role cannot be blank.");
+            askAddEmployeeQuestions();
+            return;
+        }
+
+        inquirer.prompt([
+
+            {
+                type: 'list',
+                message: `You want to add an employee named ${response.employeefirstname} ${response.employeelastname}, who works as a/an ${response.employeerole}. Is this correct?`,
+                name: 'employeeverify',
+                choices: yesOrNo
+            },
+        ])
+
+        .then (function(response) {
+            if(response.employeeverify === "Yes") {
+                console.log ("Great! We will continue.");
+            } else {
+                askAddEmployeeQuestions();
+            }
+        })
+
+    })
 }
 
 function askViewDepartmentQuestions() {
-    console.log("You want to view a department.");
+    inquirer.prompt([
+
+        {
+            type: 'list',
+            message: 'Which department would you like to view?',
+            name: 'viewdepts',
+            choices: departments
+        },
+
+    ])
+
+    .then(function (response) {
+        console.log(`Thanks for your feedback!`)
+    })
 }
 
 function askViewRoleQuestions() {
-    console.log("You want to view a role.");
+    inquirer.prompt([
+
+        {
+            type: 'list',
+            message: 'Which role would you like to view?',
+            name: 'viewroles',
+            choices: roles
+        },
+
+    ])
+
+    .then(function (response) {
+        console.log(`Thanks for your feedback!`)
+    })
 }
 
 function askViewEmployeeQuestions() {
-    console.log("You want to view an employee.");
+    inquirer.prompt([
+
+        {
+            type: 'list',
+            message: 'Which employee would you like to view?',
+            name: 'viewemployees',
+            choices: employees
+        },
+
+    ])
+
+    .then(function (response) {
+        console.log(`Thanks for your feedback!`)
+    })
 }
 
 function askUpdateEmployeeQuestions() {
-    console.log("You want to update an employee's role.");
+    inquirer.prompt([
+
+        {
+            type: 'list',
+            message: 'Which employee would you like to update?',
+            name: 'updateemployees',
+            choices: employees
+        },
+
+    ])
+
+    .then(function (response) {
+        console.log(`Thanks for your feedback!`)
+    })
 }
